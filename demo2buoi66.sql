61905d2220861353576f9521b672bd25f0e3c60b
Trantien03 <115622082+Trantien03@users.noreply.github.com>
2023-01-31 21:07:19 +0700

    demobuoi6

---
+++ demo2buoi66.sql
@@ -0,0 +1,12 @@
+3   select * from dongxe where SoChoNgoi > 5
+
+
+
+ 5   select * from nhacungcap order by TenNhaCC asc, MaSoThue desc;
+
+6   select * from dangkycungcap where NgayBatDauCungCap = '2015-11-20'
+7   select HangXe from dongxe 
+
+9   select * from dangkycungcap where dongxe_DongXe = 'Hiace' or dongxe_DongXe = 'Cerato'
+
+10  SELECT nhacungcap_MaNhaCC FROM dangkycungcap
